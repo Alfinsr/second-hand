439fdc5830ec3ee9ae2e209e626c24cd16d2d029
rizalmohamad209 <rizalmohamad209@gmail.com>
2022-06-15 20:11:00 +0700

    [RIZAL][ADD] - add entity users

--- src/main/java/com/kelompok5/secondhand/dto/UsersDto.java
+++ src/main/java/com/kelompok5/secondhand/dto/UsersDto.java
@@ -1,41 +1,21 @@
-package com.kelompok5.secondhand.entity;
+package com.kelompok5.secondhand.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.*;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-@Entity(name = "users")
-public class Users {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer idUser;
-
-    @Column
+public class UsersDto {
     private String fullName;
-
-    @Column
     private String username;
-
-    @Column
     private String email;
-
-    @Column
     private String password;
-
-    @Column
     private String noWa;
-
-    @Column
     private String alamat;
-
-    @Column
     private String kota;
 }
