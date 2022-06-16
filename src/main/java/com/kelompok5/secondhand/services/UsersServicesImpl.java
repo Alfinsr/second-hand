b102ba031f68d8a4e8c7ce88719d596764f3f678
rizalmohamad209 <rizalmohamad209@gmail.com>
2022-06-16 11:51:49 +0700

    [RIZAL][MODIFY] - modify service users and service users impl

---
+++ src/main/java/com/kelompok5/secondhand/services/UsersServicesImpl.java
@@ -0,0 +1,33 @@
+package com.kelompok5.secondhand.services;
+
+import com.kelompok5.secondhand.entity.Users;
+
+import java.util.List;
+import java.util.Optional;
+
+public class UsersServicesImpl implements UsersServices{
+    @Override
+    public Users postUsers(Users body) {
+        return null;
+    }
+
+    @Override
+    public List<Users> getAllUsers() {
+        return null;
+    }
+
+    @Override
+    public Optional<Users> getUserById(Integer id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Users updateUser(Users body, Integer id) {
+        return null;
+    }
+
+    @Override
+    public String deleteUser(Integer id) {
+        return null;
+    }
+}
