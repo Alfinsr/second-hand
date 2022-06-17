934d9f21835d4fe9309622e8cce5ab631277d71d
rizalmohamad209 <rizalmohamad209@gmail.com>
2022-06-17 18:07:31 +0700

    [RIZAL][MODIFY] - fix conflict

--- src/main/java/com/kelompok5/secondhand/controllers/UsersController.java
+++ src/main/java/com/kelompok5/secondhand/controllers/UsersController.java
@@ -1,29 +1,4 @@
 package com.kelompok5.secondhand.controllers;
 
-import com.kelompok5.secondhand.dto.UsersDto;
-import com.kelompok5.secondhand.entity.Users;
-import com.kelompok5.secondhand.services.UsersServices;
-import lombok.RequiredArgsConstructor;
-import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController @RequiredArgsConstructor
 public class UsersController {
-
-    @Autowired
-    private final UsersServices usersServices;
-
-    @Autowired
-    ModelMapper modelMapper;
-
-    @PostMapping("/signup")
-    public ResponseEntity<Users> postUsers(@RequestBody UsersDto usersDto){
-        Users users = modelMapper.map(usersDto, Users.class);
-        return new ResponseEntity<>(usersServices.postUsers(users), HttpStatus.CREATED);
-    }
 }
