f2b7c2186359640f515268360608d222427fbc09
dreamer005 <alfin.sr13@gmail.com>
2022-06-20 22:00:20 +0800

    [ALFIN][ADD] - Get Product By Id

--- src/main/java/com/kelompok5/secondhand/services/ProductServicesImpl.java
+++ src/main/java/com/kelompok5/secondhand/services/ProductServicesImpl.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.Map;
 
 @Service
@@ -32,6 +33,10 @@ public class ProductServicesImpl implements ProductServices {
     @Override
     public DataResult<List<Product>> getAllProduct() {
         return new SuccessDataResult<>(productRepository.findAll(),"success get all products") ;
+    }
+    @Override
+    public Optional<Product> getProductById(Integer id) {
+        return productRepository.findById(id);
     }
     @Override
     public Result postProduct(Product body) {
