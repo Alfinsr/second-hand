6db658852ccfc9969b9f83943faf2d19c72bef15
rizalmohamad209 <rizalmohamad209@gmail.com>
2022-07-07 23:25:45 +0700

    [RIZAL][ADD] - cors origin

--- src/main/java/com/kelompok5/secondhand/SecondHandApplication.java
+++ src/main/java/com/kelompok5/secondhand/SecondHandApplication.java
@@ -4,12 +4,25 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SecondHandApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SecondHandApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer configurer(){
+		return new WebMvcConfigurer(){
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/*")
+						.allowedOrigins("*");
+			}
+		};
 	}
 
 	@Bean
