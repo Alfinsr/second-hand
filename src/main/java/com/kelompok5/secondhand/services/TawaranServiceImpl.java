9328dd7e213ee314b75b3874c40173be9769a17a
rizalmohamad209 <rizalmohamad209@gmail.com>
2022-07-15 13:53:19 +0700

    [RIZAL][MODIFY] - modify response

--- src/main/java/com/kelompok5/secondhand/services/TawaranServiceImpl.java
+++ src/main/java/com/kelompok5/secondhand/services/TawaranServiceImpl.java
@@ -4,7 +4,9 @@ import com.kelompok5.secondhand.entity.Tawaran;
 import com.kelompok5.secondhand.repository.TawaranRepository;
 import com.kelompok5.secondhand.repository.ProductRepository;
 import com.kelompok5.secondhand.repository.UsersRepository;
+import com.kelompok5.secondhand.result.DataResult;
 import com.kelompok5.secondhand.result.ResponsTawaran;
+import com.kelompok5.secondhand.result.SuccessDataResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -29,9 +31,9 @@ public class TawaranServiceImpl implements TawaranServices{
         tawaranRepository.save(tawaran);
     }
     @Override
-    public ResponsTawaran findTawaranByIdTawaran(Integer idTawaran){
+    public SuccessDataResult findTawaranByIdTawaran(Integer idTawaran){
         Tawaran tawaran = tawaranRepository.findTawaranByIdTawaran(idTawaran);
-        return new ResponsTawaran(tawaran);
+        return new SuccessDataResult(tawaran, "Success get data tawaran by id");
     }
 
 
