485beae9c1f97d3a0a7fe93f9286b6fb4da364f3
rizalmohamad209 <rizalmohamad209@gmail.com>
2022-07-22 16:23:21 +0700

    [RIZAL][MODIFY] ddl auto create

--- src/main/java/com/kelompok5/secondhand/controllers/KategoriController.java
+++ src/main/java/com/kelompok5/secondhand/controllers/KategoriController.java
@@ -33,7 +33,7 @@ public class KategoriController {
         return new ResponseEntity<>(kategoriServices.getAllKategori(), HttpStatus.OK);
     }
 
-    //uncoment when deployment
+
 
     @GetMapping("/kategori/{id}")
     public ResponseEntity<Optional<Kategori>> getKategoriById(@PathVariable Integer id){
