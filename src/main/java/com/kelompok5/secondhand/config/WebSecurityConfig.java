b6de05f4a6d3579157f9aee781da317c20545628
rizalmohamad209 <rizalmohamad209@gmail.com>
2022-07-24 00:58:04 +0700

    [RIZAL][MODIFY] get all kategori without token

--- src/main/java/com/kelompok5/secondhand/config/WebSecurityConfig.java
+++ src/main/java/com/kelompok5/secondhand/config/WebSecurityConfig.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -14,11 +15,8 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
 
 @Configuration @EnableWebSecurity @RequiredArgsConstructor
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -33,12 +31,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         CustomAuthenticationFilter customAuthenticationFIlter = new CustomAuthenticationFilter(authenticationManagerBean());
         customAuthenticationFIlter.setFilterProcessesUrl("/signin");
-//        http.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues());
+
        http.cors();
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.authorizeRequests().antMatchers( "/signup/**").permitAll();
         http.authorizeRequests().antMatchers("/signin/**").permitAll();
+        http.authorizeRequests().antMatchers(HttpMethod.GET,"/kategori/**");
         http.authorizeRequests().antMatchers("/Product/**").permitAll();
         http.authorizeRequests().antMatchers(AUTH_WHITELIST).permitAll();
         http.authorizeRequests().anyRequest().authenticated();
@@ -47,19 +46,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
 
-//    @Bean
-//    public CorsConfigurationSource configurationSource(){
-//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//        CorsConfiguration config = new CorsConfiguration();
-//        config.addAllowedOrigin("*");
-//        config.addAllowedHeader(CorsConfiguration.ALL);
-//        config.addAllowedMethod(CorsConfiguration.ALL);
-//        source.registerCorsConfiguration("/**", config);
-//        return source;
-//
-//    }
+
     private static final String[] AUTH_WHITELIST = {
-            // -- Swagger UI v2
+
             "/v2/api-docs",
             "/swagger-resources",
             "/swagger-resources/**",
